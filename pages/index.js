bb2570406d28e4fd6d0e0891e5eea2171b3591a4
IAydedeI <a.y.dede@hotmail.com>
2020-12-04 09:20:23 +0300

    Toast eklenerek Geist test edildi

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
+import { Button, useToasts } from "@geist-ui/react";
 import Layout from "../components/layout";
-import Head from "next/head";
 
-export default function Home() {
+const Home = () => {
+  const [toasts, setToast] = useToasts();
+  const click = () =>
+    setToast({ text: "The Evil Rabbit jumped over the fence." });
+
   return (
     <Layout>
       <div>
         <h1>Ana Sayfa</h1>
       </div>
+      <Button onClick={click}>Show Toast</Button>
     </Layout>
   );
-}
+};
+
+export default Home;
