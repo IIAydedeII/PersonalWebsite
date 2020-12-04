8db9e1dce1d7bc0f5f4eec11452cdcde0bb8f619
IAydedeI <a.y.dede@hotmail.com>
2020-12-04 09:18:09 +0300

    Geist ve syled components eklendi

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import { GeistProvider, CssBaseline } from "@geist-ui/react";
 import "../styles/app.css";
-import "../styles/navigation.css";
+import "../styles/components/navigation.css";
+import "../styles/components/layout.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  );
 }
